f75615a8d0cbabe10e3a94448d55e357fee493a5
Lab <vlaicuvodarobots@gmail.com>
2021-02-16 10:17:38 +0200

    Removed deprecated Driver class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDob.java
@@ -67,14 +67,20 @@ public class DriverDob extends LinearOpMode {
              * BUTON Y*/
             if(true){
                 if((gamepad1.y==true)&&(booly1==false)) {booly2=true;}
-                if(booly2==true) {T1motor.setPower(-1);}
+                if(booly2==true) {T1motor.setPower(1);}
                 if((gamepad1.y==false)&&(booly2==true)) {booly1=true;}
                 if((gamepad1.y==true)&&(booly1==true)) {booly2=false;}
-                if(booly2==false) {T1motor.setPower(0);}
+                if(booly2==false) {T1motor.setPower(-gamepad1.left_trigger);
+                                    T1motor.setPower(gamepad1.right_trigger);} //sau 0
                 if((gamepad1.y==false)&&(booly2==false)) {booly1=false;}
                 telemetry.addData("BandaRulantaKaufland", booly2);
                 telemetry.update();
+
             }
+            /**BIND MANUAL MOVE PENTRU BANDA RULANTA KAUFLAND 1
+             * BUTON TRIGGER
+             */
+
 
             /**BIND PT MISCAREA LATERALA
              * BUTON BUMPERS*/
