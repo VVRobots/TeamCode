b4b2c4f6ac50141c036552a8313506d802cb26d5
Lab <vlaicuvodarobots@gmail.com>
2021-02-16 11:00:11 +0200

    Added claw controls prototype

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDob.java
@@ -1,21 +1,26 @@
-
+/**Copyright Dobrinel */
 package org.firstinspires.ftc.teamcode;
 
 //Librarii
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="DriverDob", group="Linear Opmode")
 public class DriverDob extends LinearOpMode {
 
     /**DECLARARE VARIABILE/OBIECTE*/
+
     private DcMotor FRmotor=null;
     private DcMotor FLmotor=null;
     private DcMotor BRmotor=null;
     private DcMotor BLmotor=null;
     private DcMotor T1motor=null;
+    private DcMotor T2motor=null;
+    private Servo SVmotor1=null;
+    private Servo SVmotor2=null;
     boolean booly1=false;
     boolean booly2=false;
 
@@ -28,27 +33,41 @@ public class DriverDob extends LinearOpMode {
         BRmotor=hardwareMap.get(DcMotor.class,"spateDreapta");
         BLmotor=hardwareMap.get(DcMotor.class,"spateStanga");
         T1motor=hardwareMap.get(DcMotor.class,"motorTraction1");
+        T2motor=hardwareMap.get(DcMotor.class,"motorBrat1");
+        SVmotor1=hardwareMap.get(Servo.class,"servoBrat1");
+        SVmotor2=hardwareMap.get(Servo.class,"servoBrat2");
+
 
         FRmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         FLmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BRmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BLmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         T1motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        T2motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
 
         FRmotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FLmotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BRmotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BLmotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         T1motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        T2motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         FRmotor.setDirection(DcMotorSimple.Direction.FORWARD);
         BRmotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         FLmotor.setDirection(DcMotorSimple.Direction.REVERSE);
         BLmotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        SVmotor1.setDirection(Servo.Direction.FORWARD);
+        SVmotor1.setPosition(0);
+        SVmotor2.setDirection(Servo.Direction.FORWARD);
+        SVmotor2.setPosition(0);
+
         telemetry.addData("Status", "Initializat");
         telemetry.update();
         /**AICI INCEPE DISTRACTIA*/
+
         waitForStart();
 
         while (opModeIsActive()) {
@@ -65,15 +84,18 @@ public class DriverDob extends LinearOpMode {
             /**BIND SWTICH PENTRU BANDA RULANTA KAUFLAND 1
              * BUTON Y*/
             if(true){
-                if((gamepad1.y==true)&&(booly1==false)) {booly2=true;}
-                if(booly2==true) {T1motor.setPower(-1);}
-                if((gamepad1.y==false)&&(booly2==true)) {booly1=true;}
-                if((gamepad1.y==true)&&(booly1==true)) {booly2=false;}
-                if(booly2==false) {T1motor.setPower(0);}
-                if((gamepad1.y==false)&&(booly2==false)) {booly1=false;}
+                if((gamepad2.y==true)&&(booly1==false)) {booly2=true;}
+                if(booly2==true) {T1motor.setPower(1);}
+                if((gamepad2.y==false)&&(booly2==true)) {booly1=true;}
+                if((gamepad2.y==true)&&(booly1==true)) {booly2=false;}
+                if(booly2==false) {T1motor.setPower(-gamepad2.left_trigger);
+                                    T1motor.setPower(gamepad2.right_trigger);} //sau 0
+                if((gamepad2.y==false)&&(booly2==false)) {booly1=false;}
                 telemetry.addData("BandaRulantaKaufland", booly2);
                 telemetry.update();
+
             }
+
 
             /**BIND PT MISCAREA LATERALA
              * BUTON BUMPERS*/
@@ -91,6 +113,19 @@ public class DriverDob extends LinearOpMode {
                     FLmotor.setPower(-1);
                     BLmotor.setPower(1);}
             }
+            /**BIND
+             * B*/
+            if(true) {
+                T2motor.setPower(-gamepad2.left_trigger);
+                T2motor.setPower(gamepad2.right_trigger);
+                if(gamepad2.left_bumper) SVmotor1.setPosition(1);
+                if(gamepad2.right_bumper) SVmotor1.setPosition(0);
+
+                if(gamepad2.a) SVmotor2.setPosition(1);
+                if(gamepad2.b) SVmotor2.setPosition(0);
+
+            }
+
 
             /**BIND PT MISCARE FINA FATA-SPATE-LATERALE
              * BUTON D-PAD*/
