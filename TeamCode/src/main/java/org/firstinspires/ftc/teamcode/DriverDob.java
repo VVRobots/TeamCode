ae60fd46ff95c967bf3f272004306aa56fedb63c
Lab <vlaicuvodarobots@gmail.com>
2021-02-19 11:54:29 +0200

    finisat bind-uri adaugat marsarier adaugat info telemetry data potentiale mici imbunatatiri bind lansator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDob.java
@@ -4,7 +4,10 @@ package org.firstinspires.ftc.teamcode;
 //Librarii
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogOutput;
+import com.qualcomm.robotcore.hardware.AnalogOutputController;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
@@ -22,8 +25,17 @@ public class DriverDob extends LinearOpMode {
     private Servo SVmotor1=null;
     private Servo SVmotor2=null;
     private Servo SVmotor3=null;
+    private int marsarier=1;
+    //private DigitalChannel sA=null;
+    //private DigitalChannel sB=null;
     boolean booly1=false;
     boolean booly2=false;
+    boolean booly10=false;
+    boolean booly20=false;
+    boolean booly100=false;
+    boolean booly200=false;
+    boolean booly1000=false;
+    boolean booly2000=false;
 
     @Override
     public void runOpMode() {
@@ -34,10 +46,12 @@ public class DriverDob extends LinearOpMode {
         BRmotor=hardwareMap.get(DcMotor.class,"spateDreapta");
         BLmotor=hardwareMap.get(DcMotor.class,"spateStanga");
         T1motor=hardwareMap.get(DcMotor.class,"motorTraction1");
-        T2motor=hardwareMap.get(DcMotor.class,"motorBrat1");
+        T2motor=hardwareMap.get(DcMotor.class,"motorTraction2");
         SVmotor1=hardwareMap.get(Servo.class,"servoBrat1");
         SVmotor2=hardwareMap.get(Servo.class,"servoBrat2");
         SVmotor3=hardwareMap.get(Servo.class,"servoBrat3");
+        //sA=hardwareMap.get(DigitalChannel.class,"semD");
+        //sB=hardwareMap.get(DigitalChannel.class, "semS");
 
 
         FRmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -64,91 +78,127 @@ public class DriverDob extends LinearOpMode {
         SVmotor1.setDirection(Servo.Direction.FORWARD);
         SVmotor1.setPosition(0);
         SVmotor2.setDirection(Servo.Direction.FORWARD);
-        SVmotor2.setPosition(0);
+        SVmotor2.setPosition(1);
         SVmotor3.setDirection(Servo.Direction.FORWARD);
         SVmotor3.setPosition(0.5);
 
         telemetry.addData("Status", "Initializat");
         telemetry.update();
         /**AICI INCEPE DISTRACTIA*/
-
         waitForStart();
 
         while (opModeIsActive()) {
             /**BIND MISCARE PRIMARA ROTI 2 CATE 2 PE LATERALE
-             * BUTON JOYSTICK AXA Y*/
-                FRmotor.setPower(gamepad1.left_stick_y);
-                BRmotor.setPower(gamepad1.left_stick_y);
-
-                FLmotor.setPower(gamepad1.right_stick_y);
-                BLmotor.setPower(gamepad1.right_stick_y);
+             * G1 BUTON JOYSTICK AXA Y*/
+            /**++MARSARIER*/
+                if((gamepad1.back==true)&&(booly100==false)) {booly200=true;}
+                if(booly200==true) {
+                    marsarier=-1;
+                    FRmotor.setPower(gamepad1.right_stick_y*marsarier);
+                    BRmotor.setPower(gamepad1.right_stick_y*marsarier);
+                    FLmotor.setPower(gamepad1.left_stick_y*marsarier);
+                    BLmotor.setPower(gamepad1.left_stick_y*marsarier); }
+                if((gamepad1.back==false)&&(booly200==true)) {booly100=true;}
+                if((gamepad1.back==true)&&(booly100==true)) {booly200=false;}
+                if(booly200==false) {
+                    marsarier=1;
+                    FRmotor.setPower(gamepad1.left_stick_y*marsarier);
+                    BRmotor.setPower(gamepad1.left_stick_y*marsarier);
+                    FLmotor.setPower(gamepad1.right_stick_y*marsarier);
+                    BLmotor.setPower(gamepad1.right_stick_y*marsarier);}
+                if((gamepad1.back==false)&&(booly200==false)) {booly100=false;}
 
             /**BIND SWTICH PENTRU BANDA RULANTA KAUFLAND 1
-             * BUTON Y/JOISTICK*/
+             * G2 BUTON Y/JOISTICK*/
                 if((gamepad2.y==true)&&(booly1==false)) {booly2=true;}
                 if(booly2==true) {T1motor.setPower(1);}
                 if((gamepad2.y==false)&&(booly2==true)) {booly1=true;}
                 if((gamepad2.y==true)&&(booly1==true)) {booly2=false;}
                 if(booly2==false) {T1motor.setPower(gamepad2.left_stick_y);}
                 if((gamepad2.y==false)&&(booly2==false)) {booly1=false;}
-                telemetry.addData("BandaRulantaKaufland", booly2);
-                telemetry.update();
+
+            /**BIND SWTICH PENTRU LANSATOR PRODUSE KAUFLAND 2
+             * G2 BUTON A*/
+            if((gamepad2.a==true)&&(booly1000==false)) {booly2000=true;}
+            if(booly2000==true) {T2motor.setPower(1);}
+            if((gamepad2.a==false)&&(booly2000==true)) {booly1000=true;}
+            if((gamepad2.a==true)&&(booly1000==true)) {booly2000=false;}
+            if(booly2000==false) {T2motor.setPower(0);}
+            if((gamepad2.a==false)&&(booly2000==false)) {booly1000=false;}
 
             /**BIND PT MISCAREA LATERALA
-             * BUTON BUMPERS*/
+             * G1 BUTON BUMPERS*/
                 if (gamepad1.left_bumper) {                 /**MISCARE LATERALA STANGA*/
-                    FRmotor.setPower(-1);                   /**BIND LEFT BUMPER*/
-                    BRmotor.setPower(1);
-                    FLmotor.setPower(1);
-                    BLmotor.setPower(-1);}
-
+                    FRmotor.setPower(-1*marsarier);                   /**BIND LEFT BUMPER*/
+                    BRmotor.setPower(1*marsarier);
+                    FLmotor.setPower(1*marsarier);
+                    BLmotor.setPower(-1*marsarier);}
                 if (gamepad1.right_bumper) {                /**MISCARE LATERALA DREAPTA*/
-                    FRmotor.setPower(1);                    /**BIND RIGHT BUMPER*/
-                    BRmotor.setPower(-1);
-                    FLmotor.setPower(-1);
-                    BLmotor.setPower(1);}
-
-                    FRmotor.setPower(0);                    /**RESETARE VALORI*/
-                    BRmotor.setPower(0);
-                    FLmotor.setPower(0);
-                    BLmotor.setPower(0);
+                    FRmotor.setPower(1*marsarier);                    /**BIND RIGHT BUMPER*/
+                    BRmotor.setPower(-1*marsarier);
+                    FLmotor.setPower(-1*marsarier);
+                    BLmotor.setPower(1*marsarier);}
+                FRmotor.setPower(0);                    /**RESETARE VALORI*/
+                BRmotor.setPower(0);
+                FLmotor.setPower(0);
+                BLmotor.setPower(0);
 
             /**BIND BRAT
-             * G2 BUMPERS/A/B/X/JOYSTICK*/
-                if(gamepad2.left_bumper) SVmotor2.setPosition(0.5);     /**DESCHIDERE CLESTE*/
-                if(gamepad2.right_bumper) SVmotor2.setPosition(1);      /**INCHIDERE CLESTE*/
-                if(gamepad2.x) SVmotor1.setPosition(0);         /**ROTATIE CAP DE BRAT*/
-                if(gamepad2.a) SVmotor1.setPosition(0.5);
-                if(gamepad2.b) SVmotor1.setPosition(1);
-                SVmotor3.setPosition(gamepad2.right_stick_y/2+0.5);     /**RIDICARE/COBORARE BRAT*/
+             * G2 BUMPERS/DPAD LEFT-UP-RIGHT/JOYSTICK*/
+                if(gamepad2.dpad_left) SVmotor1.setPosition(0);         /**ROTATIE CAP DE BRAT*/
+                if(gamepad2.dpad_up) SVmotor1.setPosition(0.7);
+                if(gamepad2.dpad_right) SVmotor1.setPosition(1);
+                SVmotor3.setPosition(-gamepad2.right_stick_y/2+0.5);     /**RIDICARE/COBORARE BRAT*/
+
+                if(gamepad2.left_bumper) SVmotor1.setPosition(SVmotor1.getPosition()-0.002);
+                if(gamepad2.right_bumper) SVmotor1.setPosition(SVmotor1.getPosition()+0.002);
+
+            if((gamepad2.x==true)&&(booly10==false)) {booly20=true;}
+            if(booly20==true) {SVmotor2.setPosition(0.4);;}
+            if((gamepad2.x==false)&&(booly20==true)) {booly10=true;}
+            if((gamepad2.x==true)&&(booly10==true)) {booly20=false;}
+            if(booly20==false) {SVmotor2.setPosition(1);;}
+            if((gamepad2.x==false)&&(booly20==false)) {booly10=false;}
 
             /**BIND PT MISCARE FINA FATA-SPATE-LATERALE
-             * BUTON D-PAD*/
+             * G1 BUTON D-PAD*/
 
                 if (gamepad1.dpad_up){
-                    FRmotor.setPower(-0.25);
-                    BRmotor.setPower(-0.25);
-                    FLmotor.setPower(-0.25);
-                    BLmotor.setPower(-0.25);}
+                    FRmotor.setPower(-0.3*marsarier);
+                    BRmotor.setPower(-0.3*marsarier);
+                    FLmotor.setPower(-0.3*marsarier);
+                    BLmotor.setPower(-0.3*marsarier);}
                 if (gamepad1.dpad_down){
-                    FRmotor.setPower(0.25);
-                    BRmotor.setPower(0.25);
-                    FLmotor.setPower(0.25);
-                    BLmotor.setPower(0.25);}
+                    FRmotor.setPower(0.3*marsarier);
+                    BRmotor.setPower(0.3*marsarier);
+                    FLmotor.setPower(0.3*marsarier);
+                    BLmotor.setPower(0.3*marsarier);}
                 if (gamepad1.dpad_left){
-                    FRmotor.setPower(-0.25);
-                    BRmotor.setPower(0.25);
-                    FLmotor.setPower(0.25);
-                    BLmotor.setPower(-0.25);}
+                    FRmotor.setPower(-0.3*marsarier);
+                    BRmotor.setPower(0.3*marsarier);
+                    FLmotor.setPower(0.3*marsarier);
+                    BLmotor.setPower(-0.3*marsarier);}
                 if (gamepad1.dpad_right){
-                    FRmotor.setPower(0.25);
-                    BRmotor.setPower(-0.25);
-                    FLmotor.setPower(-0.25);
-                    BLmotor.setPower(0.25);}
+                    FRmotor.setPower(0.3*marsarier);
+                    BRmotor.setPower(-0.3*marsarier);
+                    FLmotor.setPower(-0.3*marsarier);
+                    BLmotor.setPower(0.3*marsarier);}
             FRmotor.setPower(0);
             BRmotor.setPower(0);
             FLmotor.setPower(0);
             BLmotor.setPower(0);
-        }
-    }
-}
+
+
+
+            if(booly100==false) telemetry.addData("Gearbox","1st Gear");
+            if(booly100==true) telemetry.addData("Gearbox","Reverse");
+            if(booly2==false) telemetry.addData("Ramp","Manual");
+            if(booly2==true) telemetry.addData("Ramp","Full Throttle");
+            if(booly2000==false) telemetry.addData("Launcher","Off");
+            if(booly2000==true) telemetry.addData("Launcher","Full Throttle");
+
+            telemetry.update();
+
+        }   /**while (opModeIsActive())*/
+    }   /**RunOpMode*/
+}   /**LinearOpMode*/
