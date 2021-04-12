38502b5be72a9703e49f030bd56595523bbf3dbf
widz <tavaandrei@gmail.com>
2021-04-12 17:11:51 +0300

    Autonomous OP Mode Blind Prototype V1.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterAuto.java
@@ -20,28 +20,113 @@ public class MasterAuto extends LinearOpMode {
     private final double actualCountsPerRev = countsPerMotorRev * gearRed;
     private final double countsPerCM = actualCountsPerRev / circumference;
     private final double robotLength = 36;
-    private final double robotWidth = 27;
+    private final double robotWidth = 23;
     private final double robotDiameter = Math.sqrt(robotLength*robotLength+robotWidth*robotWidth);
     private final double robotCircumference = Math.PI*robotDiameter;
     private int nrRings = 0;
+    private int servoStart=;   //Daca nu setati direct servoul modificati aici pana da bine
+    private int servoEnd=;
 
+    private DcMotor RampMotor = null;
+    private DcMotor LauncherMotor = null;
     private DcMotor FRwheelMotor = null;
     private DcMotor FLwheelMotor = null;
     private DcMotor BRwheelMotor = null;
     private DcMotor BLwheelMotor = null;
+    private Servo LauncherServo = null;
+    private ColorRangeSensor lowSensor =null;
+    private Rev2mDistanceSensor highSensor = null;
 
+    Thread Fire = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            LauncherServo.setPosition(servoEnd);
+            sleep(1500);
+            LauncherServo.setPosition(servoStart);
+        }
+    });
+
+    /**Autonomia presupune ca se incepe de pe linia din stanga, cu lansatorul in fata
+    * !!! CODUL NU O SA MEARGA IN STAREA CURENTA, trebuie completat cu numele
+     * din config ale componentelor in Initialize() si pozitiile bune ale servoului mai sus,
+     * la servoStart si End
+     *
+    * Unde se modifica pentru tuning:
+     * -In runOpMode()(functia principala), toate distantele si vitezele, chiar si
+     * instructiunile in sine. VALORILE MELE SUNT DATE ORIENTATIV, SANSE EXTREM DE MARI
+     * SA FIE PROASTE.
+     * -la blocul de variabile, servoStart si servoEnd drept pozitiile servoului
+     * -durata de sleep din diversele functii
+     * -Scenario A, B, C pentru diversele zone de dropat Wobbleul
+     * (DropWobble nu face nimic, dar l-am pus sa incerce sa se duca acolo sa vad cum merge)
+     * ca sa nu mai incerce asta comentati tot switchul
+     *
+     * Codul presupune ca senzorii sunt montati, daca vreti fara ei comentati liniile
+     * relevante din Initialize() si elinimati functia ScanRings() din runOpMode()
+     **/
 
 
     @Override
     public void runOpMode()
     {
 
+        Initialize();
 
+        telemetry.addData("Status:", "Gata");
+        telemetry.update();
+        waitForStart();
+        DriveRight(0.8,15);
+        DriveForward(0.8,55);
+
+        nrRings=ScanRings();
+
+        DriveLeft(0.8,60);
+
+        LauncherMotor.setPower(1);
+
+        DriveForward(0.8,135);
+        Launch(1);
+        TurnLeft(0.5,5); //sau pur si simplu DriveLeft
+        Launch(1);
+        TurnLeft(0.5,5); //sau pur si simplu DriveLeft
+        Launch(1);
+
+        LauncherMotor.setPower(0);
+
+        TurnRight(0.5,10); //omisa daca s-a facut Drive si nu Turn
+        DriveForward(0.8,15);
+
+        switch (nrRings)
+        {
+            case 0:
+                ScenarioA();
+                break;
+            case 1:
+                ScenarioB();
+                break;
+            case 4:
+                ScenarioC();
+                break;
+        }
+
+
+
+    }
+
+    public void Initialize()
+    {
         FRwheelMotor = hardwareMap.get(DcMotor.class,"fataDreapta");
         FLwheelMotor = hardwareMap.get(DcMotor.class,"fataStanga");
         BRwheelMotor = hardwareMap.get(DcMotor.class,"spateDreapta");
         BLwheelMotor = hardwareMap.get(DcMotor.class,"spateStanga");
 
+        RampMotor = hardwareMap.get(DcMotor.class,"NUME DIN CONFIG");
+        LauncherMotor = hardwareMap.get(DcMotor.class,"NUME DIN CONFIG");
+
+        LauncherServo = hardwareMap.get(Servo.class,"NUME SERVO DIN CONFIG ");
+
+        lowSensor =  hardwareMap.get(ColorRangeSensor.class, "NUME SENZOR CULOARE");
+        highSensor = hardwareMap.get(Rev2mDistanceSensor.class, "NUME SENZOR DISTANTA");
 
         FRwheelMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FLwheelMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -60,6 +145,9 @@ public class MasterAuto extends LinearOpMode {
         BRwheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         BLwheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        RampMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LauncherMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
         FRwheelMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         BRwheelMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -67,49 +155,70 @@ public class MasterAuto extends LinearOpMode {
         FLwheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         BLwheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        RampMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        LauncherMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-
-
-
-        telemetry.addData("Status:", "Gata");
-        telemetry.update();
-        waitForStart();
-
-        TurnRight(0.69,90);
-
-
+        LauncherServo.setPosition(servoStart);
 
 
     }
 
-    public void DriveForward(double speed, double distanceCM)
+    public void ScenarioA()
     {
-        DriveTo(speed,distanceCM,distanceCM,distanceCM,distanceCM);
+        DriveRight(0.8,135);
+        DropWobble();
     }
-    public void DriveBackwards(double speed, double distanceCM)
+
+    public void ScenarioB()
     {
-        DriveTo(speed,-distanceCM,-distanceCM,-distanceCM,-distanceCM);
+        DriveRight(0.8,75);
+        DriveForward(0.8,60);
+        DropWobble();
+        DriveBackwards(0.8,60);
     }
-    public void DriveRight(double speed, double distanceCM)
+
+    public void ScenarioC()
     {
-        DriveTo(speed,-distanceCM,distanceCM,distanceCM,-distanceCM);
+        DriveRight(0.8,135);
+        DriveForward(0.8,120);
+        DropWobble();
+        DriveBackwards(0.8,120);
     }
-    public void DriveLeft(double speed, double distanceCM)
+
+    public int ScanRings()
     {
-        DriveTo(speed,distanceCM,-distanceCM,-distanceCM,distanceCM);
+        if(lowSensor.getDistance(DistanceUnit.CM)<10)
+            if(highSensor.getDistance(DistanceUnit.CM)<10)
+                return 4;
+            else
+                return 1;
+        else
+            return 0;
     }
-    public void TurnRight(double speed, double degrees)
+
+    public void Launch(int nofRings)
     {
-        double arcRatio=degrees/360;
-        double distanceCM = robotCircumference*arcRatio;
-        DriveTo(speed,-distanceCM,-distanceCM,distanceCM,distanceCM);
+        if(!LauncherMotor.isBusy())
+        {
+            LauncherMotor.setPower(1);
+            sleep(1000);
+        }
+        while(nofRings>0)
+        {
+            if(!Fire.isAlive())
+            {
+                Fire.start();
+                nofRings--;
+            }
+        }
+        sleep(500);
     }
-    public void TurnLeft(double speed, double degrees)
+
+    public void DropWobble()
     {
-        double arcRatio=degrees/360;
-        double distanceCM = (robotCircumference*arcRatio);
-        DriveTo(speed,distanceCM,distanceCM,-distanceCM,-distanceCM);
+
     }
+
     public void DriveTo(double speed, double frontRightCM, double backRightCM, double frontLeftCM, double backLeftCM)
     {
 
@@ -172,5 +281,33 @@ public class MasterAuto extends LinearOpMode {
             sleep(200);
         }
 
+    }
+    public void DriveForward(double speed, double distanceCM)
+    {
+        DriveTo(speed,distanceCM,distanceCM,distanceCM,distanceCM);
+    }
+    public void DriveBackwards(double speed, double distanceCM)
+    {
+        DriveTo(speed,-distanceCM,-distanceCM,-distanceCM,-distanceCM);
+    }
+    public void DriveRight(double speed, double distanceCM)
+    {
+        DriveTo(speed,-distanceCM,distanceCM,distanceCM,-distanceCM);
+    }
+    public void DriveLeft(double speed, double distanceCM)
+    {
+        DriveTo(speed,distanceCM,-distanceCM,-distanceCM,distanceCM);
+    }
+    public void TurnRight(double speed, double degrees)
+    {
+        double arcRatio=degrees/360;
+        double distanceCM = robotCircumference*arcRatio;
+        DriveTo(speed,-distanceCM,-distanceCM,distanceCM,distanceCM);
+    }
+    public void TurnLeft(double speed, double degrees)
+    {
+        double arcRatio=degrees/360;
+        double distanceCM = (robotCircumference*arcRatio);
+        DriveTo(speed,distanceCM,distanceCM,-distanceCM,-distanceCM);
     }
 }
